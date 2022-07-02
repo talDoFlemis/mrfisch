3c8b529111b61e9ad71b51e384567ec3c3fd401d
Flemis <coderflemis@gmail.com>
2022-07-02 16:27:37 -0300

    adding dashboard layout

---
+++ components/layout/DashboardLayout.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { HiCode } from "react-icons/hi";
+import { MdOutlineCreate, MdSettings } from "react-icons/md";
+import { GiFriedFish } from "react-icons/gi";
+import cl from "clsx";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const { pathname } = useRouter();
+  return (
+    <div className="flex h-screen  bg-[#141627] font-momcake antialiased">
+      <div className="drawer-mobile drawer text-white">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex">{children}</div>
+        <aside className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu w-48 items-center justify-center overflow-y-auto bg-[#141627] p-4 text-base-content">
+            <Link href="/">
+              <a className="absolute top-4 font-spaceRave text-xl italic text-white hover:text-red-500">
+                Mr fisch
+                <GiFriedFish className="mx-auto h-8 w-8" />
+              </a>
+            </Link>
+            <div className="flex flex-col text-lg text-slate-300">
+              <Link href="/codes/">
+                <a
+                  className={cl(
+                    "btn flex cursor-pointer items-center justify-start space-x-2 rounded-md border-none py-2 transition-colors hover:bg-red-500/80",
+                    pathname === "/codes" && " text-white"
+                  )}
+                >
+                  <HiCode className="h-6 w-6" />
+                  <p>All Codes</p>
+                </a>
+              </Link>
+              <Link href="/create/">
+                <a
+                  className={cl(
+                    "btn flex cursor-pointer items-center justify-start space-x-4 rounded-md border-none py-2 transition-colors hover:bg-red-500/80",
+                    pathname === "/create" && " text-white"
+                  )}
+                >
+                  <MdOutlineCreate className="h-6 w-6" />
+                  <p>create</p>
+                </a>
+              </Link>
+              <Link href="/settings/">
+                <a
+                  className={cl(
+                    "btn flex cursor-pointer items-center justify-start space-x-4 rounded-md border-none py-2 transition-colors hover:bg-red-500/80",
+                    pathname === "/settings" && " text-white"
+                  )}
+                >
+                  <MdSettings className="h-6 w-6" />
+                  <p>Settings</p>
+                </a>
+              </Link>
+            </div>
+          </ul>
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
