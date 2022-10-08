44a1348ed5939b5bd56952b1022b405456df0d81
taldoflemis <coderflemis@gmail.com>
2022-10-08 16:13:35 -0300

    updating the testing suite for path alias and eslint shit

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-    "^@/layout/(.*)$": "<rootDir>/components/layout/$1",
-    "^@/svgs/(.*)$": "<rootDir>/components/svgs/$1",
-    "^@/utils/(.*)$": "<rootDir>/utils/$1",
-    "^@/tests/(.*)$": "<rootDir>/__tests__/$1",
+    "^@components/(.*)$": "<rootDir>/components/$1",
+    "^@functions/(.*)$": "<rootDir>/functions/$1",
+    "^@styles/(.*)$": "<rootDir>/styles/$1",
+    "^@pages/(.*)$": "<rootDir>/pages/$1",
   },
   testEnvironment: "jest-environment-jsdom",
-  modulePathIgnorePatterns: ["cypress"],
 };
 
 module.exports = createJestConfig(customJestConfig);
